2afd2c1770c5bb2fc70111ddc4de379a0623246d
Prajwal K <prajwals@eoxvantage.in>
2021-07-31 02:57:33 +0530

    [REFACTOR] Support automatic Tab UUID generation

--- gui/src/components/App/TabSegment.js
+++ gui/src/components/App/TabSegment.js
@@ -1,9 +1,8 @@
 import React from "react";
-import JsxParser from "react-jsx-parser";
-import moment from "moment";
+import { v4 } from "uuid";
 import PageContent from "./PageContent";
 import { Tabs, TabLink, TabContent } from "react-tabs-redux";
-import "./Styles/TabStyles.scss"
+import "./Styles/TabStyles.scss";
 
 class TabSegment extends React.Component {
   constructor(props) {
@@ -21,105 +20,120 @@ class TabSegment extends React.Component {
       content: this.props.content,
       pageContent: [],
       dataReady: false,
-      currentRow: this.props.currentRow?this.props.currentRow:{},
-      fileData: this.props.fileData?this.props.fileData : {},
+      currentRow: this.props.currentRow ? this.props.currentRow : {},
+      fileData: this.props.fileData ? this.props.fileData : {},
       tabNames: [],
       tabContent: [],
-      tabs: this.props.tabs?this.props.tabs:[]
+      tabs: this.props.tabs ? this.props.tabs : [],
     };
-    if(this.props.tabs.length > 1){
+    if (this.props.tabs.length > 1) {
       var tabNames = [];
-      var tabContent = []
+      var tabContent = [];
       this.props.tabs.map((item, i) => {
-        tabNames.push(<TabLink to={item.uuid}> {item.name}</TabLink>);
-        var tabContentKey = item.uuid+'_tab';
-        var fileData = item.fileData? item.fileData : {};
-        tabContent.push(<TabContent for={item.uuid} key={item.uuid} visibleClassName="visibleTabStyle">
-        <PageContent
-          key={tabContentKey}
-          config={this.props.config}
-          proc={this.props.proc}
-          isTab="true"
-          appId={this.props.appId}
-          parentPage={this.pageId}
-          fileData={fileData}
-          currentRow={this.state.currentRow}
-          pageContent={item.content}
-          pageId={item.pageId}
-          fileId={this.fileId}
-          core={this.core}
-        />
-            </TabContent>)
+        let uuidv4 = v4();
+        tabNames.push(
+          <TabLink to={item.uuid ? item.uuid : uuidv4}> {item.name}</TabLink>
+        );
+        var tabContentKey = (item.uuid ? item.uuid : uuidv4) + "_tab";
+        var fileData = item.fileData ? item.fileData : {};
+        tabContent.push(
+          <TabContent
+            for={item.uuid ? item.uuid : uuidv4}
+            key={item.uuid ? item.uuid : uuidv4}
+            visibleClassName="visibleTabStyle"
+          >
+            <PageContent
+              key={tabContentKey}
+              config={this.props.config}
+              proc={this.props.proc}
+              isTab="true"
+              appId={this.props.appId}
+              parentPage={this.pageId}
+              fileData={fileData}
+              currentRow={this.state.currentRow}
+              pageContent={item.content}
+              pageId={item.pageId}
+              fileId={this.fileId}
+              core={this.core}
+            />
+          </TabContent>
+        );
       });
-      this.state.tabNames= tabNames;
-      this.state.tabContent= tabContent;
-      this.state.dataReady= true;
+      this.state.tabNames = tabNames;
+      this.state.tabContent = tabContent;
+      this.state.dataReady = true;
     }
   }
-  componentDidUpdate(prevProps){
-    if(prevProps.tabs !== this.props.tabs){
-      this.setState({tabs:this.props.tabs});
-      if(this.props.tabs.length > 1){
+  componentDidUpdate(prevProps) {
+    if (prevProps.tabs !== this.props.tabs) {
+      this.setState({ tabs: this.props.tabs });
+      if (this.props.tabs.length > 1) {
         var tabNames = [];
-        var tabContent = []
+        var tabContent = [];
         this.props.tabs.map((item, i) => {
-          tabNames.push(<TabLink to={item.uuid}> {item.name}</TabLink>);
-          var tabContentKey = item.uuid+'_tab';
-          tabContent.push(<TabContent for="uuid" key={uuid}>
-          <PageContent
-            key={tabContentKey}
-            config={this.props.config}
-            proc={this.props.proc}
-            isTab="true"
-            appId={this.props.appId}
-            parentPage={this.pageId}
-            pageContent={item.content}
-            pageId={this.pageId}
-            fileId={this.fileId}
-            currentRow={this.state.currentRow}
-            core={this.core}
-          />
-              </TabContent>)
+          let uuidv4 = v4();
+          tabNames.push(
+            <TabLink to={item.uuid ? item.uuid : uuidv4}> {item.name}</TabLink>
+          );
+          var tabContentKey = (item.uuid ? item.uuid : uuidv4) + "_tab";
+          tabContent.push(
+            <TabContent
+              for={item.uuid ? item.uuid : uuidv4}
+              key={item.uuid ? item.uuid : uuidv4}
+            >
+              <PageContent
+                key={tabContentKey}
+                config={this.props.config}
+                proc={this.props.proc}
+                isTab="true"
+                appId={this.props.appId}
+                parentPage={this.pageId}
+                pageContent={item.content}
+                pageId={this.pageId}
+                fileId={this.fileId}
+                currentRow={this.state.currentRow}
+                core={this.core}
+              />
+            </TabContent>
+          );
         });
-        this.setState({tabNames: tabNames});
-        this.setState({tabContent: tabContent});
+        this.setState({ tabNames: tabNames });
+        this.setState({ tabContent: tabContent });
       }
     }
   }
 
   render() {
-    if (
-      this.state.tabs &&
-      this.state.tabs.length == 1
-    ) {
-        return (<PageContent
+    if (this.state.tabs && this.state.tabs.length == 1) {
+      return (
+        <PageContent
           key={this.state.tabs[0].uuid}
           config={this.props.config}
           proc={this.props.proc}
           appId={this.props.appId}
           fileId={this.fileId}
-          pageContent={this.state.tabs[0].content?this.state.tabs[0].content:null}
+          pageContent={
+            this.state.tabs[0].content ? this.state.tabs[0].content : null
+          }
           currentRow={this.state.currentRow}
           core={this.core}
-        />);
-  } else if(this.state.tabs &&
-  this.state.dataReady){
-    return (<Tabs
-              name="tabs2"
-              className="tabs"
-              selectedTab={this.state.tabs[0].uuid}
-            >
-          <div className="links">
-{this.state.tabNames}
-                    </div>
-          <div className="tabContentDiv">
-{this.state.tabContent}
-          </div>
-            </Tabs>);
-  } else {
+        />
+      );
+    } else if (this.state.tabs && this.state.dataReady) {
+      return (
+        <Tabs
+          name="tabs2"
+          className="tabs"
+          selectedTab={this.state.tabs[0].uuid}
+        >
+          <div className="links">{this.state.tabNames}</div>
+          <div className="tabContentDiv">{this.state.tabContent}</div>
+        </Tabs>
+      );
+    } else {
       return <div>No Content to Display</div>;
+    }
   }
-}
 }
 
 export default TabSegment;
