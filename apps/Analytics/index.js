ba7b483d9ca8d77c5b76a72bd1db8af37553f833
bhanupriyac <bhanupriyac@eoxvantage.in>
2021-08-24 11:54:02 +0530

    [REQ]25921-Change the icons on the apps to the Fontawesome icons (Admin, Analytics, MailAdmin)

--- apps/Analytics/index.js
+++ apps/Analytics/index.js
@@ -1,58 +1,72 @@
-import './index.scss';
-import osjs from 'osjs';
-import {React,ReactDOM} from "oxziongui"
-import metaData from './metadata.json';
+import "./index.scss";
+import osjs from "osjs";
+import { React, ReactDOM } from "oxziongui";
+import metaData from "./metadata.json";
 // import {name as applicationName} from './metadata.json';
-import icon_white from './metadata.json';
-import Body from './body'
+import icon_white from "./metadata.json";
+import fontIcon from "./metadata.json";
+import Body from "./body";
 
 // Our launcher
 const register = (core, args, options, metadata) => {
   // Create a new Application instance
-  const proc = core.make('osjs/application', {args, options, metadata});
+  const proc = core.make("osjs/application", { args, options, metadata });
   // Create  a new Window instance
-  let session = core.make('osjs/settings').get('osjs/session');
+  let session = core.make("osjs/settings").get("osjs/session");
   let sessions = Object.entries(session);
-  var i, finalposition, finalDimension,finalMaximised,finalMinimised, win;
+  var i, finalposition, finalDimension, finalMaximised, finalMinimised, win;
   for (i = 0; i < sessions.length; i++) {
-    if (Object.keys(session[i].windows).length && session[i].name == metadata.name){
+    if (
+      Object.keys(session[i].windows).length &&
+      session[i].name == metadata.name
+    ) {
       finalposition = session[i].windows[0].position;
       finalDimension = session[i].windows[0].dimension;
       finalMaximised = session[i].windows[0].maximized;
       finalMinimised = session[i].windows[0].minimized;
     }
   }
-  var win = proc.createWindow({
+  var win = proc
+    .createWindow({
       id: metadata.name + "_Window",
       title: metadata.title.en_EN,
-      icon: proc.resource(icon_white),
+      // icon: proc.resource(icon_white),
+      icon: metadata.fontIcon,
       attributes: {
         classNames: ["Window_" + metadata.name],
-        dimension: finalDimension ? finalDimension : {
-          width: 900,
-          height: 570
-        },
+        dimension: finalDimension
+          ? finalDimension
+          : {
+              width: 900,
+              height: 570,
+            },
         minDimension: {
           width: 900,
-          height: 570
+          height: 570,
         },
-        position: finalposition ? finalposition : {
-          left: 150,
-          top: 50
-        }
-      }
-    }).on('destroy', () => proc.destroy())
-    .on('render', (e) => { win.maximize(); })
-    .render($content => ReactDOM.render(<Body args={core} proc={proc}/>, $content));
-    if(finalMinimised){
-      win.minimize();
-    }
-    if(finalMaximised){
+        position: finalposition
+          ? finalposition
+          : {
+              left: 150,
+              top: 50,
+            },
+      },
+    })
+    .on("destroy", () => proc.destroy())
+    .on("render", (e) => {
       win.maximize();
-    }
+    })
+    .render(($content) =>
+      ReactDOM.render(<Body args={core} proc={proc} />, $content)
+    );
+  if (finalMinimised) {
+    win.minimize();
+  }
+  if (finalMaximised) {
+    win.maximize();
+  }
   return proc;
 };
 
 // Creates the internal callback function when OS.js launches an application
 osjs.register(metaData.name, register);
-
