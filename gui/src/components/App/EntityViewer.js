bde2a8fbb309734ba90f04e3572eb2696cd0f8c7
mukuld <mukuld@eoxvantage.in>
2021-09-04 19:19:56 +0530

    Task-app header changes

--- gui/src/components/App/EntityViewer.js
+++ gui/src/components/App/EntityViewer.js
@@ -50,7 +50,7 @@ class EntityViewer extends React.Component {
   callAuditLog() {
     this.setState({ showAuditLog: true });
   }
-  generateEditButton(enableComments, enableAuditLog) {
+  generateEditButton(enableComments, enableAuditLog, fileData) {
     var fileId;
     let gridToolbarContent = [];
     var filePage;
@@ -61,25 +61,66 @@ class EntityViewer extends React.Component {
         fileId = this.state.fileId;
       }
     }
+    if(this.appId === '454a1ec4-eeab-4dc2-8a3f-6a4255ffaee1'){ //TASK APP
+      gridToolbarContent.push(this.getTaskHeader(fileData))
+    }
     if (this.state.entityConfig && !this.state.entityConfig.has_workflow) {
       filePage = [{ type: "Form", form_id: this.state.entityConfig.form_uuid, name: this.state.entityConfig.form_name, fileId: fileId }];
       let pageContent = { pageContent: filePage, title: "Edit", icon: "far fa-pencil" }
-      gridToolbarContent.push(<Button title={"Edit"} className={"toolBarButton"} primary={true} onClick={(e) => this.updatePageContent(pageContent)} ><i className={"fa fa-pencil"}></i></Button>);
+      gridToolbarContent.push(<Button title={"Edit"} className={"btn btn-primary"} primary={true} onClick={(e) => this.updatePageContent(pageContent)} ><i className={"fa fa-pencil"}></i></Button>);
     }
-    gridToolbarContent.push(<Button title={"Print"} className={"toolBarButton"} primary={true} onClick={(e) => this.callPrint()} ><i className={"fa fa-print"}></i></Button>);
+    gridToolbarContent.push(<Button title={"Print"} className={"btn btn-primary"} primary={true} onClick={(e) => this.callPrint()} ><i className={"fa fa-print"}></i></Button>);
     if (enableAuditLog) {
-      gridToolbarContent.push(<Button title={"Audit Log"} className={"toolBarButton"} primary={true} onClick={(e) => this.callAuditLog()} ><i className={"fa fa-history"}></i></Button>);
+      gridToolbarContent.push(<Button title={"Audit Log"} className={"btn btn-primary"} primary={true} onClick={(e) => this.callAuditLog()} ><i className={"fa fa-history"}></i></Button>);
     }
     if (enableComments != "0") {
       var commentPage = { title: "Comments", icon: "far fa-comment", pageContent: [{ type: "Comment", fileId: fileId }] };
-      gridToolbarContent.push(<Button title={"Comments"} className={"toolBarButton"} primary={true} onClick={(e) => this.updatePageContent(commentPage)} ><i className={"fa fa-comment"}></i></Button>);
+      gridToolbarContent.push(<Button title={"Comments"} className={"btn btn-primary"} primary={true} onClick={(e) => this.updatePageContent(commentPage)} ><i className={"fa fa-comment"}></i></Button>);
     }
-    gridToolbarContent.push(<Button title={"Generate Link"} className={"toolBarButton"} primary={true} onClick={(e) => this.core.make("oxzion/link").copyToClipboard('<a eoxapplication="' + this.state.entityConfig.app_name + '" file-id="' + fileId + '" href="' + this.core.config('ui.url') + '?app=' + this.state.entityConfig.app_name + '&fileId=' + fileId + '" >Link</a>')} ><i className={"fa fa-share-alt"}></i></Button>);
+    gridToolbarContent.push(<Button title={"Generate Link"} className={"btn btn-primary"} primary={true} onClick={(e) => this.core.make("oxzion/link").copyToClipboard('<a eoxapplication="' + this.state.entityConfig.app_name + '" file-id="' + fileId + '" href="' + this.core.config('ui.url') + '?app=' + this.state.entityConfig.app_name + '&fileId=' + fileId + '" >Link</a>')} ><i className={"fa fa-share-alt"}></i></Button>);
     let ev = new CustomEvent("addcustomActions", {
       detail: { customActions: gridToolbarContent },
       bubbles: true,
     });
     document.getElementById(this.appId + "_breadcrumbParent").dispatchEvent(ev);
+  }
+
+  getTaskHeader(fileData){
+    console.log(JSON.stringify(fileData.data))
+    const breadCrumb = document.getElementById(this.appId + "_breadcrumbParent")?.children[0]?.classList;
+    breadCrumb?.add('display-flex');
+    breadCrumb?.add('width-100');
+    const { data : { title, data : { status, start_date, next_action_date } } } = fileData;
+    return <div className="task-header">
+      <i className="fa fa-arrow-from-left go-back"></i>
+      <div className="task-header_taskname">{title.split(' ').slice(0,2).map(v => v[0].toUpperCase()).join('')}</div>
+      <div className="task-header_info">
+        <div className="task-header_name">
+          {title}
+        </div>
+        <div className="task-header_details">
+          <div><p>Status</p> <span className="task-status"></span> <p>{status}</p></div>
+          <div><p>Created On</p> <p>{start_date}</p></div>
+          <div><p>Due On</p>  <p>{next_action_date}</p></div>
+          <div className="owner-assignee">Owner <i className="fa fa-user owner-assignee-dp"></i></div>
+          <div className="owner-assignee">Assigned To <i className="fa fa-user owner-assignee-dp"></i></div>
+          <div className="task-header_progress">
+            <div className="task-header_progress--data">
+              <div>
+                <p><strong>10H</strong> Done</p>
+                <p><strong>26H</strong> To do</p>
+              </div>
+              <div>
+                <div style={{width:"60%", background : 'red', padding:'1.5px', margin : 0}}></div>
+                <div style={{width:"30%", background : 'green', padding:'1.5px', margin : 0}}></div>
+                <div style={{width:"10%", background : '#888', padding:'1.5px', margin : 0}}></div>
+              </div>
+            </div>
+            <p className="display-flex"><strong>30%</strong>{" "}Done</p>
+          </div>
+        </div>
+      </div>
+    </div>
   }
 
   componentDidMount() {
@@ -88,8 +129,11 @@ class EntityViewer extends React.Component {
         var file = fileData.data.data ? fileData.data.data : fileData.data;
         this.setState({ entityId: fileData.data.entity_id, fileData: file });
         this.getEntityPage().then(entityPage => {
+          console.log(`entityPage.data`);
+          console.log(entityPage)
+          console.log(fileData)
           this.setState({ entityConfig: entityPage.data });
-          this.generateEditButton(entityPage.data.enable_documents, entityPage.data.enable_auditlog);
+          this.generateEditButton(entityPage.data.enable_documents, entityPage.data.enable_auditlog, fileData);
           var content = this.constructTabs(entityPage.data, entityPage.data.enable_documents, entityPage.data.enable_view);
           this.setState({ content: content });
           this.setState({ dataReady: true });
