4097f87cd2aae9507657e5ae2d1f673e429121bb
Danish Sultan <danishsultan@eoxvantage.in>
2021-09-08 16:45:09 +0000

    [DEPLOYMENT] Runtime ownership changes
    (cherry picked from commit 3a2c8d61719efb40e5b40dd77f418cd54540e313)
    
    (cherry picked from commit e27f75437a365b1508eb3f78bb02e6eda11458ae)

--- docker/run.sh
+++ docker/run.sh
@@ -23,7 +23,7 @@ then
 	sed -ri -e "s/([0-9]{1,3}\.){3}[0-9]{1,3}:8080/${IP}:8080/" ./bos/src/server/local.js
 
 	if [ ! -f ./view_built ]; then
-		ls ./view_built >> /dev/null 2>&1 && echo "Starting view" || (echo "Building view" && ./build.sh gui && ./build.sh iconpacks && ./build.sh themes && ./build.sh apps Admin,Announcements,Preferences && ./build.sh bos && touch ./view_built)
+		ls ./view_built >> /dev/null 2>&1 && echo "Starting view" || (echo "Building view" && chown root:root gui/ -R && ./build.sh gui && ./build.sh iconpacks && ./build.sh themes && ./build.sh apps Admin,Announcements,Preferences && ./build.sh bos && touch ./view_built)
 		touch ./view_built
 	fi
 
