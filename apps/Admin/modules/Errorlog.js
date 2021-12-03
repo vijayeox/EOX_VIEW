3023939de829811d466f3f52a515ed416fe9b110
bhanupriyac <bhanupriyac@eoxvantage.in>
2021-12-03 19:55:40 +0530

    [REQ]27410-New List view(Admin-Goal,Admin-ErrorLog)

--- apps/Admin/modules/Errorlog.js
+++ apps/Admin/modules/Errorlog.js
@@ -1,111 +1,110 @@
-import { React, OX_Grid } from "oxziongui";
+import { React, EOXGrid } from "oxziongui";
 import { TitleBar } from "./components/titlebar";
-
+import { GetData } from "./components/apiCalls";
 class Errorlog extends React.Component {
   constructor(props) {
     super(props);
     this.core = this.props.args;
+    this.drillDownRequired = false;
+    (this.actionItems = {
+      retry:{
+        type:"button",
+        api:"errorlog",
+        icon:"fad fa-redo",
+        text:"RETRY",
+        ispopup:true,
+      }
+    }),
+    (this.state = {
+      isLoading: true,
+      accountData: [],
+      
+      permission: {
+          canAdd: this.props.userProfile.privileges.MANAGE_TEAM_CREATE,
+          canEdit: this.props.userProfile.privileges.MANAGE_TEAM_WRITE,
+          canDelete: this.props.userProfile.privileges.MANAGE_TEAM_DELETE,
+        },
+    }),
+    (this.api = "errorlog")
   }
 
-  async retryCall(dataItem) {
-    let helper2 = this.core.make("oxzion/restClient");
-    let response = await helper2.request(
-      "v1",
-      "errorlog/" + dataItem.id + "/retry",
-      {},
-      "post"
-    );
-    return response;
-  }
-
-  renderButtons(e, action) {
-    var actionButtons = [];
-    Object.keys(action).map(function (key, index) {
-      var buttonStyles = action[key].icon
-        ? {
-            width: "2.2rem",
-          }
-        : {
-            width: "auto",
-            paddingTop: "5px",
-            color: "white",
-            fontWeight: "600",
-          };
-      actionButtons.push(
-        <abbr title={action[key].name} key={index}>
-          <button
-            type="button"
-            className=" btn btn-primary manage-btn k-grid-edit-command"
-            onClick={() => this.retryCall(e)}
-            style={buttonStyles}
-          >
-            {action[key].icon ? (
-              <i className={action[key].icon + " manageIcons"}></i>
-            ) : (
-              action[key].name
-            )}
-          </button>
-        </abbr>
-      );
-    }, this);
-    return actionButtons;
-  }
-
-  renderEmpty() {
-    return [<React.Fragment key={1} />];
+  componentDidMount() {
+      GetData(this.api).then((data) => {
+      this.setState({
+        accountData: (data.status === "success" && data.data) || [],
+        isLoading: false,
+      });
+    });
+  
   }
 
   render() {
+    let config = {
+      height: "100%",
+      width: "100%",
+      filterable: true,
+      reorderable: true,
+      sortable: true,
+      // sort:true,
+      pageSize: 4,
+      // pageable:true,
+      pageable: {
+        skip: 0,
+        // pageSize: 10,
+        buttonCount: 3,
+      },
+      groupable: true,
+      resizable: true,
+
+      isDrillDownTable: true,
+
+      column: [
+        {
+          title: "ID",
+          field: "id",
+        },
+        {
+          title: "Type",
+          field: "error_type",
+        },
+        {
+          title: "Payload",
+          field: "payload",
+        },
+        {
+          title: "Parameters",
+          field: "params",
+        },
+        {
+          title: "Date Created",
+          field: "date_created",
+        },
+      ],
+    };
+
     return (
       <div style={{ height: "inherit" }}>
-        <TitleBar
+         <TitleBar
           title="Manage System Errors"
           menu={this.props.menu}
           args={this.core}
         />
-        <div style={{ width: "100%", padding: "7px" }}></div>
-        <OX_Grid
-          osjsCore={this.core}
-          data={"errorlog"}
-          filterable={true}
-          reorderable={true}
-          resizable={true}
-          sortable={true}
-          columnConfig={[
-            {
-              title: "ID",
-              field: "id",
-            },
-            {
-              title: "Type",
-              field: "error_type",
-            },
-            {
-              title: "Payload",
-              field: "payload",
-            },
-            {
-              title: "Parameters",
-              field: "params",
-            },
-            {
-              title: "Date Created",
-              field: "date_created",
-            },
-            {
-              title: "Actions",
-              cell: (e) =>
-                this.renderButtons(e, [
-                  {
-                    name: "Retry",
-                    rule: "true",
-                    icon: "fa fa-redo",
-                  },
-                ]),
-              filterCell: (e) => this.renderEmpty(),
-            },
-          ]}
-        />
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <div >
+          {!this.state.isLoading && (
+              <EOXGrid
+                configuration={config}
+                data={this.state.accountData}
+                core={this.core}
+                isDrillDownTable={this.props.drillDownRequired}
+                actionItems={this.actionItems}
+                api={this.api}
+                // key={Math.random()}
+              />
+            )}
+          </div>
+        </React.Suspense>
+        {/* {this.state.userInEdit && this.inputTemplate} */}
       </div>
     );
   }
