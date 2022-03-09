fea3db7be7cdd41ddba074d9a79708f97d6988d6
Rakshith <rakshith@vantageagora.com>
2022-03-09 18:19:40 +0530

    [REFACTOR] Moved all the OI related changes to its components

--- gui/index.js
+++ gui/index.js
@@ -1,9 +1,9 @@
 import EOXApplication from "./src/EOXApplication";
 import LeftMenuTemplate from "./src/LeftMenuTemplate";
 import Notification from "./src/Notification";
-import React, { lazy, Suspense } from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss';
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
 import DateComponent from "./src/components/DateComponent.js";
 import CurrencySelect from "./src/components/Currency Select/currencySelect";
 import countryStateList from "./src/components/data/country-state-codes";
@@ -20,37 +20,44 @@ import * as KendoReactWindow from "@progress/kendo-react-dialogs";
 import * as KendoReactGrid from "@progress/kendo-react-grid";
 import * as KendoReactInput from "@progress/kendo-react-inputs";
 // import * as KendoReactRipple from "@progress/kendo-react-ripple";
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { fal } from '@fortawesome/pro-light-svg-icons'
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { fal } from "@fortawesome/pro-light-svg-icons";
 
 import * as Moment from "moment";
 import * as MomentTZ from "moment-timezone";
-import * as PopupDialog from 'sweetalert2';
+import * as PopupDialog from "sweetalert2";
 import * as ReactStrap from "reactstrap";
 // import * as Webcam from "react-webcam";
 import AvatarImageCropper from "react-avatar-image-cropper";
 import GridTemplate from "./src/GridTemplate";
-import OX_Grid from "./src/OX_Grid";
-import DashboardManager from "./src/DashboardManager";
-import Dashboard from "./src/Dashboard";
-import DataSource from "./src/DataSource";
-import Query from "./src/Query";
+import OX_Grid from "./src/components/OI/OX_Grid";
+import DashboardManager from "./src/components/OI/DashboardManager";
+import Dashboard from "./src/components/OI/Dashboard";
+import DataSource from "./src/components/OI/DataSource";
+import Query from "./src/components/OI/Query";
 import PageContent from "./src/components/App/PageContent";
-import DashboardFilter from "./src/DashboardFilter";
-import WidgetGrid from "./src/WidgetGrid";
-import WidgetRenderer from "./src/WidgetRenderer";
+import DashboardFilter from "./src/components/OI/DashboardFilter";
+import WidgetGrid from "./src/components/OI/WidgetGrid";
+import WidgetRenderer from "./src/components/OI/WidgetRenderer";
 import DocumentList from "./src/DocumentList";
 import * as Antd from "antd";
 import * as AntdIcons from "@ant-design/icons";
-import Visualization from "./src/Visualization";
-import WidgetManager from "./src/WidgetManager";
-import TemplateManager from "./src/TemplateManager";
+import Visualization from "./src/components/OI/Visualization";
+import WidgetManager from "./src/components/OI/WidgetManager";
+import TemplateManager from "./src/components/OI/TemplateManager";
 import SSOCustom from "./src/components/Custom/SSOCustom";
-import EOXGrid from "./src/components/EOXGrid/EOXGrid"
+import EOXGrid from "./src/components/EOXGrid/EOXGrid";
 //import Mapbox from "./src/components/Custom/Mapbox";
 
 const LazyLoad = ({ component: Component, ...rest }) => (
-  <React.Suspense fallback={<div className="spinner"><div className="bounce1"></div><div className="bounce2"></div><div className="bounce3"></div></div>}>
+  <React.Suspense
+    fallback={
+      <div className='spinner'>
+        <div className='bounce1'></div>
+        <div className='bounce2'></div>
+        <div className='bounce3'></div>
+      </div>
+    }>
     <Component {...rest} />
   </React.Suspense>
 );
@@ -68,14 +75,14 @@ const MultiSelect = (props) => <LazyLoad component={lazy(() => import("./src/Mul
 const HTMLViewer = (props) => <LazyLoad component={lazy(() => import("./src/components/App/HTMLViewer"))} {...props} />;
 const CommentsView = (props) => <LazyLoad component={lazy(() => import("./src/components/App/CommentsView"))} {...props} />;
 const DocumentViewer = (props) => <LazyLoad component={lazy(() => import("./src/DocumentViewer"))} {...props} />;
-const DateFormats = (props) => <LazyLoad component={lazy(() => import('./src/public/js/DateFormats.js'))} {...props} />;
-const DropDown = (props) => <LazyLoad component={lazy(() => import('./src/components/Dropdown/DropDownList'))} {...props} />;
+const DateFormats = (props) => <LazyLoad component={lazy(() => import("./src/public/js/DateFormats.js"))} {...props} />;
+const DropDown = (props) => <LazyLoad component={lazy(() => import("./src/components/Dropdown/DropDownList"))} {...props} />;
 //const Visualization = (props) => <LazyLoad component={lazy(() => import("./src/Visualization"))} {...props} />;
 //const WidgetManager = (props) => <LazyLoad component={lazy(() => import("./src/WidgetManager"))} {...props} />;
 
 // const OX_Grid = lazy(() => import("./src/OX_Grid"));
 // const GridTemplate = lazy(() => import("./src/GridTemplate"));
-library.add(fal)
+library.add(fal);
 
 export {
   EOXApplication,
