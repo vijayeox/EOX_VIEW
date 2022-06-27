4dd5b3ea75cadcbb30a1c560a98adb7a08cce90b
Karan Agarwal <karan@eoxvantage.com>
2022-06-27 23:03:09 +0530

    [REFACTOR] Simplify docker startup | _ issue fix

--- start.sh
+++ start.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-dirName="$(tr [A-Z] [a-z] <<< "${PWD##*/}")"
-echo "Stopping container if already running..."
-docker stop "${dirName//_}-vw-1"
+execOptions() {
+	containerName=`docker container ls -la -f name="$dirName" --format "{{.Names}}"`
+	containerStatus=`docker container inspect --format="{{.State.Status}}" $containerName`
+	if [ "$containerStatus" != "exited" ]; then
+		while true; do
+			case $startOptions in
+				[Ii]* ) echo "==================================================";
+						echo "			Logs";
+						echo "==================================================";
+						docker logs -n100 -f "$containerName";
+						break;;
+				[Yy]* ) docker exec -it "$containerName" bash; break;;
+				[Nn]* ) break;;
+					* ) read -p "Do you wish to enter the container or display logs?(y/n/i)" startOptions;;
+			esac
+		done
+		exit
+	fi
+}
 
 IP=`hostname -I | awk '{ print $1 }'`
 
@@ -17,18 +33,30 @@ do
 	esac
 done
 
+dirName="$(tr [A-Z] [a-z] <<< "${PWD##*/}")";
+containerName=`docker container ls -la -f name="$dirName" --format "{{.Names}}"`
+if [ "$containerName" ]; then
+	containerStatus=`docker container inspect --format="{{.State.Status}}" $containerName`
+	if [ "$containerStatus" != "exited" ] && [ "$startOptions" == "i" ] || [ "$startOptions" == "y" ]; then
+		currentIp=`docker exec -it $containerName printenv -0 IP`
+		echo ""
+		if [ "$currentIp" == "$IP" ]; then
+			read -p "Do you want to restart the service?(y/n)" restart
+			if [ "$restart" == "n" ]; then
+				execOptions startOptions containerName
+			fi
+		fi
+	fi
+	echo "Stopping container if already running..."
+	docker stop "$containerName"
+fi
+
 chmod 777 -R ./docker/run.sh
 
-if [ "$startOptions" == "i" ] || [ ! -f ./view_built ]; then
-	IP="$IP" docker-compose up --build
-else
-	IP="$IP" docker-compose up -d --build
-	echo "View is being served in the background on port 8081."
-	while true; do
-		case $startOptions in
-			[Yy]* ) docker exec -it "${dirName//_}-vw-1" bash; break;;
-			[Nn]* ) break;;
-				* ) read -p "Do you wish to enter the container?(y/n)" startOptions;;
-		esac
-	done
+if [ ! -f ./view_built ]; then
+	startOptions="i"
 fi
+
+IP="$IP" docker-compose up -d --build
+echo "View is being served in the background on port 8081."
+execOptions startOptions containerName
