c55affaefd395fe4c8bfcde7a11c7e62fca6e4cb
Prajwal K Divate <prajwals@eoxvantage.in>
2022-07-12 21:33:01 +0530

    [Task] #37582 - Refactor UserSessionAdapter

--- bos/src/client/adapters/UserSessionAdapter.js
+++ bos/src/client/adapters/UserSessionAdapter.js
@@ -17,29 +17,41 @@ export class UserSessionServiceProvider extends ServiceProvider {
       get: () => this.get(),
       set: () => this.setUserSession(),
     }));
+    this.lsHelper = new LocalStorageAdapter();
   }
 
   async get() {
     let helper = this.core.make("oxzion/restClient");
     let session = await helper.request("v1", "/user/me/getsession", {}, "get");
-    this.core.make("osjs/settings").set("osjs/session", null, session["data"]).save();
+    this.core
+      .make("osjs/settings")
+      .set("osjs/session", null, session["data"])
+      .save();
 
     let userSession = await helper.request("v1", "/user/me", {}, "GET");
-    if (userSession["data"]["cleared_browser_cache"] == 1) {
+    if (userSession?.data?.cleared_browser_cache == 1) {
       // clearCacheData();
       caches.keys().then((names) => {
         names.forEach((name) => {
           caches.delete(name);
         });
       });
-      localStorage.clear();
+
+      this.lsHelper.supported();
+
+      this.lsHelper.purge("UserInfo");
+      this.lsHelper.purge("Metadata");
+
+      // this.core.make("oxzion/profile").set();
+      // this.core.make("oxzion/profile").getMetadata();
+
       localStorage.setItem("version", "New");
       window.location.reload(true);
 
-      //Update the user cache value to 0
+      // Update the user cache value to 0
       await helper.request(
         "v1",
-        "/user/" + userSession["data"]["id"],
+        "/user/" + userSession?.data?.id,
         JSON.stringify({
           cleared_browser_cache: 0,
         }),
@@ -51,6 +63,11 @@ export class UserSessionServiceProvider extends ServiceProvider {
   async setUserSession() {
     let session = this.core.make("osjs/settings").get("osjs/session");
     let helper = this.core.make("oxzion/restClient");
-    let updatesession = await helper.request("v1", "/user/me/updatesession", { data: JSON.stringify(session) }, "post");
+    let updatesession = await helper.request(
+      "v1",
+      "/user/me/updatesession",
+      { data: JSON.stringify(session) },
+      "post"
+    );
   }
 }
